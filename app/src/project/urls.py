d7bd78a027e87646211432af627bb35492ca4bd3
Michał Nowacki <michal.nowacki@reef.pl>
2025-05-01 14:58:48 +0200

    redirect from / to /simulator/

--- app/src/project/urls.py
+++ app/src/project/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.contrib.admin.sites import site
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 from .core.consumers import DefaultConsumer
 from .core.views import simulation_view, simulate_single_case_view
@@ -8,6 +9,7 @@ from .core.views import simulation_view, simulate_single_case_view
 urlpatterns = [
     path("admin/", site.urls),
     path("", include("django.contrib.auth.urls")),
+    path("", RedirectView.as_view(url="/simulator/", permament=False)),
     path("simulator/", simulation_view, name="simulation_view"),
     path("simulate_single_case/", simulate_single_case_view, name="simulate_single_case"),
 ]
